f1b1812b96ff6bdd86bf0879ecc3e12c3a39af5b
RHBoyJY <46002103+RHBoyJY@users.noreply.github.com>
2024-05-06 17:40:05 +0800

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -5,7 +5,7 @@ with st.sidebar:
     # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     openai_api_key = st.secrets.API_Key
     if not openai_api_key:
-        st.info("OpenAI API key failed loading")
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     #"[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     #"[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
  
